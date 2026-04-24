998618f9bbe68295259084c0cae1152ddb83f510
서연 <seoyeon020784@gmail.com>
2026-04-24 16:05:18 +0900

    객체 정의/생성/메서드 호출
    1. 선언/정의 : 구분해서 작성필요, 함수 선언/정의 시 아래 구성대로 구현필요
    `-(return type)함수이름:(파라미터 타입)파라미터이름;
    
    2. 생성: 아래 구성대로 생성
    `객체이름 *변수명 = [객체이름 new]`
    
    3. 메서드 호출: [Reciever Message] 구성
    `[객체변수명 메서드명:파라미터값];`

--- FirstProject/FirstProject/main.m
+++ FirstProject/FirstProject/main.m
@@ -7,18 +7,44 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Vehicle: NSObject
+@interface Vehicle: NSObject {
+    // member variable
+    // 여기는 선언부이기때문에 초기화 X
+    int wheels;
+    int seats;
+}
+// member method
+// 여기도 선언부이기때문에 구현 X
+-(void)setWheels:(int)w;
+-(void)sedSeats:(int)s;
+-(void)print;
 @end
 
 @implementation Vehicle
+- (void)sedSeats:(int)s {
+    seats = s;
+}
+
+- (void)setWheels:(int)w {
+    wheels = w;
+}
+
+- (void)print {
+    NSLog(@"wheels : %i, seats: %i", wheels, seats);
+}
+
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        int wheel = 4; // int float double long - primitive type
-        int seats = 2;
-        NSLog(@"wheels : %i, seats: %i", wheel, seats);
+        Vehicle *hello = [Vehicle new]; // create instance object
+        
+        // [Receiver Message]
+        // Message == Method, send/sent = invoke
+        // Receiver == Vehicle, Message == new
+        [hello setWheels:4];
+        [hello sedSeats:2];
+        [hello print];
     }
     return EXIT_SUCCESS;
 }
